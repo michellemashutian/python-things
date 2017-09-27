0db314af69de5bf65d0d3cde9fecb3488e588c19
mashutian <mashutian0608@hotmail.com>
2017-09-27 16:29:32 +0800

    Update gensim-build-dictionary.py

--- gensim-build-dictionary.py
+++ gensim-build-dictionary.py
@@ -9,7 +9,7 @@ logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=lo
 
 '''
 # for several documents
-fn=['alluser_final']
+fn=['XX']
 for f in fn: 
     doc=open('E:\\'+f+'.txt','r').read().strip().decode('gbk').split('\n')#.decode('gbk').split('\n')
     texts = [[word for word in document.lower().split(' ')] for document in doc]
@@ -22,9 +22,9 @@ print 'hello'
 '''
 
 
-doc=open('E:\\deal\\asist2017\\enall\\enall.txt','r').read().strip().split('\n')#.strip()
+doc=open('E:\\XX.txt','r').read().strip().split('\n')#.strip()
 texts = [[word for word in document.lower().split(' ')] for document in doc]
 dictionary = corpora.Dictionary(texts)
-dictionary.save('E:\\deal\\asist2017\\enall\\enall.dict')
+dictionary.save('E:\\XX.dict')
 corpus = [dictionary.doc2bow(text) for text in texts]
-corpora.MmCorpus.serialize('E:\\deal\\asist2017\\enall\\enall.mm',corpus)
+corpora.MmCorpus.serialize('E:\\XX.mm',corpus)
