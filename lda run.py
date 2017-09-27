599b963397f5a44ded39c768bb455b9de8ae6fd9
mashutian <mashutian0608@hotmail.com>
2017-09-27 16:37:23 +0800

    Update lda run.py

--- lda run.py
+++ lda run.py
@@ -18,3 +18,21 @@ for i in range(1):
         matrix.append(' '.join(vec))
     f.write('\n'.join(matrix))
     f.close()
+'''
+filelist=['enall']
+for files in filelist:
+    doc=open('E:\\deal\\asist2017\\'+files+'\\'+files+'.txt','r').read().split('\n')#.decode('gbk').split('\n')
+    corpus=CountVectorizer().fit_transform(doc)
+    for i in range(20):
+        ss=10*(i+1)
+        model = lda.LDA(n_topics=ss, n_iter=2000, random_state=1)
+        model.fit(corpus)
+        matrix=[] 
+        f=open('E:\\deal\\asist2017\\'+files+'\\lda\\'+files+'_lda%s.vec'%ss,'w')
+        for i,term in enumerate(model.doc_topic_):
+            print i
+            vec=[str(terms) for terms in term]
+            matrix.append(' '.join(vec))
+        f.write('\n'.join(matrix))
+        f.close()
+ '''
